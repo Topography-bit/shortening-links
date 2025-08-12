9e09f49a5574d4e4161f480b39af0427abaae808
Kirill Muzyka <kiruhahaj9@gmail.com>
2025-08-12 21:00:54 +0300

    Исправлена синтаксическая ошибка

--- app/links/redisdao.py
+++ app/links/redisdao.py
@@ -11,25 +11,26 @@ class RedisLinksDAO:
 
     @staticmethod
     async def get_link_cached(short_code: str):
-            cached_url = await redis_client.get(short_code)
-            if cached_url:
-                data = json.loads(cached_url)
-                clicks = await LinksDAO.add_click(short_code)
-                return data["long_url"]
-            
-            long_url = await LinksDAO.find_long_url(short_code)
-            if not long_url:
-                raise HTTPException(status_code=404, detail="Ссылка не найдена")
-                
+        cached_url = await redis_client.get(short_code)
+        if cached_url:
+            data = json.loads(cached_url)
             clicks = await LinksDAO.add_click(short_code)
+            return data["long_url"]
+            
+        long_url = await LinksDAO.find_long_url(short_code)
+        if not long_url:
+            raise HTTPException(status_code=404, detail="Ссылка не найдена")
+                
+        clicks = await LinksDAO.add_click(short_code)
 
-            clicks_amount = await LinksDAO.how_many_clicks(short_code)
+        clicks_amount = await LinksDAO.how_many_clicks(short_code)
 
-            if clicks_amount >= 5:
-                cache_data = {
-                    "long_url": long_url,
-                    "clicks": clicks
-                }
-                await redis_client.set(short_code, json.dumps(cache_data), ex=86400)
-            return long_url
+        if clicks_amount >= 5:
+            cache_data = {
+                "long_url": long_url,
+                "clicks": clicks
+            }
+            await redis_client.set(short_code, json.dumps(cache_data), ex=86400)
+            
+        return long_url
     
